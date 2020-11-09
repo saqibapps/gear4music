f9bf2fa4b5addcb1e1c49821f9280e9fa559330d
Saqib Ilyas <saqib.job.apps@gmail.com>
2020-11-09 17:50:32 +0000

    Working draft
    Inital thoughts

---
+++ batchprocessing.php
@@ -0,0 +1,28 @@
+<?php
+
+new BatchProcessing();
+
+
+class BatchProcessing {
+	
+	//open a new batch file
+	public function openBatchFile( ) {
+
+	}
+
+	/*
+	*  function to add single consignment to batchfile
+	*/
+	public function addToBatchfile( $consignment ) {
+
+	}
+
+	public function closeBatchFile( $file ) {
+
+	}
+
+	
+	use view controller for more control and structure.
+
+
+}
---
+++ file.php
@@ -0,0 +1,23 @@
+<?php
+
+class File {
+
+	public file;
+	public filehandle;
+
+
+	public function openfile( ) {
+		// open file handle
+		// $this ->filehandle
+	}
+
+	public function appendTofile(  ) {
+
+	}
+
+	public function closefile(  ) {
+
+	}
+
+}
+
